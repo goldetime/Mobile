6c7af372ed8a7f9efc8e8b1689b742af851e0c90
goldetime <ochirsum74@gmail.com>
2019-10-18 21:28:56 +0800

    add two fragments

--- lab6/app/src/main/java/mn/num/edu/fragment/MainActivity.java
+++ lab6/app/src/main/java/mn/num/edu/fragment/MainActivity.java
@@ -1,14 +1,19 @@
 package mn.num.edu.fragment;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
 
 public class MainActivity extends AppCompatActivity {
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_main);
+		setContentView(R.layout.activity_main);
   }
 }
